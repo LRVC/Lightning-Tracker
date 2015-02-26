6a6036bc7adb28aa877b9ab703ea86c1e2420680
LRVC <lrv378@gmail.com>
2015-02-25 22:37:08 -0700

    User can see index page, sign up, sign in, and sign out

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+  get '/sign-in' => 'authentication#new', as: :signin
+  post '/sign-in' => 'authentication#create'
+  get '/sign-out' => 'authentication#destroy', as: :signout
+end
